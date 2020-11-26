eca230e6e61b26c21ae8c99344f8db4c74024399
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-11-27 00:14:33 +0300

    Create index.php
    Demonstration of using my autoload.php script

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+/**
+ * Подключим autload для работы namespace'ов
+ */
+require_once('autoload.php');
+
+
+
+/**
+ * Подключим namespace'ы
+ */
+
+use Core\Bootstrap;
+
+
+
+/**
+ * Создадим объекты для проверки моего autoload
+ */
+$bootstrap = new Bootstrap();
