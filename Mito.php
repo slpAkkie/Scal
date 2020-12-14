44b231619c8d6c8c1c520a294bf3207637f543e2
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-12-14 20:20:48 +0300

    Move debug functions add new constants and rename other constants

--- Mito.php
+++ Mito.php
@@ -36,7 +36,7 @@ function Mito( string $input ) : void
   static $Mito_conf = false;
 
   /** Mito_conf еще не была подгружена */
-  $Mito_conf === false && $Mito_conf = json_decode( @file_get_contents( __DIR__ . _DS . 'Mito.conf.json' ), true );
+  $Mito_conf === false && $Mito_conf = json_decode( @file_get_contents( MITO_CONF . MITO_DS . 'Mito.conf.json' ), true );
   /** Файл конфигурации отсутствует */
   $Mito_conf === null && $Mito_conf = array();
 
@@ -44,15 +44,15 @@ function Mito( string $input ) : void
 
 
 
-  /** Разбиение входной строки по _NS (Разделитель namespace`ов) */
-  $input = explode( _NS, $input );
+  /** Разбиение входной строки по MITO_NS (Разделитель namespace`ов) */
+  $input = explode( MITO_NS, $input );
 
   /**
    * Namespace подключаемого класса
    *
    * @var string
    */
-  $namespace = join( _DS, array_slice( $input, 0, -1, true ) );
+  $namespace = join( MITO_DS, array_slice( $input, 0, -1, true ) );
 
   /**
    * Часть namespace`а не входящая в проверку и сохраняющаяся при подстановке
@@ -81,9 +81,9 @@ function Mito( string $input ) : void
 
   if ( !$found && $namespace === '' ) {
     /** Нормализация пути */
-    $path = _DS;
+    $path = MITO_DS;
     /** Формирование пути к файлу */
-    $include_path = __DIR__ . $path . $class . _FEXT;
+    $include_path = MITO_PRD . $path . $class . MITO_FEXT;
 
     /** Если файл есть */
     if ( file_exists( $include_path ) ) {
@@ -110,9 +110,9 @@ function Mito( string $input ) : void
 
         foreach ( $paths as $i => $path ) {
           /** Нормализация пути */
-          $path = _DS . trim( preg_replace( '/\\//', _DS, $path ), _DS ) . _DS;
+          $path = MITO_DS . trim( preg_replace( '/\\//', MITO_DS, $path ), MITO_DS ) . MITO_DS;
           /** Формирование пути к файлу */
-          $include_path = __DIR__ . $path . $saved_path . $class . _FEXT;
+          $include_path = MITO_PRD . $path . $saved_path . $class . MITO_FEXT;
 
           /** Если файл есть */
           if ( file_exists( $include_path ) ) {
@@ -131,8 +131,8 @@ function Mito( string $input ) : void
   } while (
     !$found
     && preg_match( '/\\\/', $namespace )
-    && ($saved_path = join( null, array_slice( explode( '\\', $namespace ), -1, null, true ) ) . _DS)
-    && ($namespace = join( _DS, array_slice( explode( '\\', $namespace ), 0, -1, true )) )
+    && ($saved_path = join( null, array_slice( explode( '\\', $namespace ), -1, null, true ) ) . MITO_DS)
+    && ($namespace = join( MITO_DS, array_slice( explode( '\\', $namespace ), 0, -1, true )) )
   );
 }
 
@@ -156,43 +156,14 @@ spl_autoload_register( 'Mito' );
  */
 
 /** Разделитель каталогов */
-define( '_DS', DIRECTORY_SEPARATOR );
+define( 'MITO_DS', DIRECTORY_SEPARATOR );
 /** Разделитель namespace`ов */
-define( '_NS', '\\' );
+define( 'MITO_NS', '\\' );
 /** Расширение подключаемых файлов php */
-define( '_FEXT', '.php' );
+define( 'MITO_FEXT', '.php' );
 
+/** Корневая директория проекта */
+!defined( 'MITO_PRD' ) && define( 'MITO_PRD', __DIR__ );
 
-
-
-
-/**
- *
- * ====================
- * Функции для отладки
- * ====================
- *
- */
-
-/**
- * Вывести информацию о переменной
- */
-function vd( $var )
-{
-
-  echo '<pre style="font-size: 24px">';
-  var_dump( $var );
-  echo '</pre>';
-
-}
-
-/**
- * Вывести информацию о переменной и завершить выполнение скрипта
- */
-function vde( $var )
-{
-
-  vd( $var );
-  exit;
-
-}
+/** Путь к файлу конфигурации */
+!defined( 'MITO_CONF' ) && define( 'MITO_CONF', __DIR__ );
