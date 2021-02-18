15147177d77858db6b4c3ec7d4eee27385a08f3a
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-02-18 19:32:27 +0300

    Add: processing of two parameters
    * --port ot -P followed by the port number on which server will be opened
    * --open or -O to open your browser in the page with server running

--- serve.bat
+++ serve.bat
@@ -8,10 +8,42 @@
 
 
 
+
+
 @echo off
+setlocal EnableDelayedExpansion
+chcp 65001 > nul
+
 title PHP Development Server
 
-if "%~1" NEQ "" (set port=%1) else (set port=8000)
+:: Default values
+set port=8000
+set openBrowser=false
 
-start http://localhost:%port%
+
+
+:checkParameter
+set setPort=false
+
+:: If need to set another port different from defalut
+if "%~1" EQU "-P" set setPort=true
+if "%~1" EQU "--port" set setPort=true
+
+if %setPort% EQU true if "%~2" NEQ "" set port=%2
+
+:: If need to open browser with localhost tab
+if "%~1" EQU "-O" set openBrowser=true
+if "%~1" EQU "--open" set openBrowser=true
+
+:: Make shift to look next parameter
+shift
+:: If we have one more parameter let's check it
+if "%~1" NEQ "" goto checkParameter
+
+
+
+:: Open browser if need and start the server
+echo По окончанию openBrowser = %openBrowser%
+echo По окончанию port = %port%
+if %openBrowser% EQU true start http://localhost:%port%
 php -S localhost:%port%
