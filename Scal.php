6d2b0d04526bb32e6bdfd50b732468958ea644a6
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-02-18 22:21:39 +0300

    Add: Defines and includes and spl_autoload_register

--- Scal.php
+++ Scal.php
@@ -14,5 +14,27 @@
 |
 */
 
-// Define the path in which Scal was included
-define('SCAL_EXECUTED_IN', realpath(''));
+// Determine the directory where the Scal was executed
+define('SCAL_EXECUTED_IN', realpath('') . DIRECTORY_SEPARATOR);
+
+// Define Scal paths
+define('SCAL_REAL_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+define('SCAL_SUPPORT_PATH', SCAL_REAL_PATH . 'Support/');
+define('SCAL_EXCEPTIONS_PATH', SCAL_REAL_PATH . 'Exceptions/');
+
+// Use next lines if you developing Scal
+require_once SCAL_SUPPORT_PATH . 'Debug.php';
+define('SCAL_TEST_PATH', SCAL_REAL_PATH . 'Tests/');
+
+
+
+// Including Scal files
+require_once SCAL_EXCEPTIONS_PATH . 'BaseException.php';
+require_once SCAL_EXCEPTIONS_PATH . 'ClassNotFoundException.php';
+require_once SCAL_EXCEPTIONS_PATH . 'ConfigurationNotFoundException.php';
+require_once SCAL_EXCEPTIONS_PATH . 'FileNotFoundException.php';
+require_once SCAL_EXCEPTIONS_PATH . 'FolderNotFoundException.php';
+
+require_once SCAL_REAL_PATH . 'Loader.php';
+
+spl_autoload_register('Scal\Loader::load');
