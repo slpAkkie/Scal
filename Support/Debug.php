e09409c03174f5e4bbcf2f6f9f1d140fc8e5d2e0
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-02-26 20:11:43 +0300

    Update: refactor
    Make it as class

--- Support/Debug.php
+++ Support/Debug.php
@@ -3,41 +3,42 @@
 /*
 |
 |--------------------------------------------------
-| Debug functions
+| Class for debugging
 |--------------------------------------------------
-|
-| Use these functions when you need to display
-| something on the screen
 |
 */
 
-/**
- * Display variable entry and stop the script
- *
- * @param mixed $var
- * @return void
- */
-function svd( $var ): void
-{
-  print_r($var); exit;
-}
+namespace Scal\Support;
 
-/**
- * Display variable entry with its type
- * and stop the script
- *
- * @param mixed $var
- * @return void
- */
-function svt( $var ): void
-{
-  var_dump($var); exit;
-}
+class Debug {
+  /**
+   * Prints human-readable information about a variable and stop the script
+   *
+   * @return void
+   */
+  public static function log(...$vars): void
+  {
+    print_r($vars);
+    exit;
+  }
+  /**
+   * Prints human-readable information about a variable
+   *
+   * @return void
+   */
+  public static function print(...$vars): void
+  {
+    print_r($vars);
+  }
 
-/**
- * Display class name with message when it was successfuly included
- */
-function successfulyIncluded($class_name)
-{
-  echo '<div><span style="display: inline-block; width: 32%">' . $class_name . '</span><span>Успешно подключен</span></div>';
+  /**
+   * Dumps information about a variable and stop the script
+   *
+   * @return void
+   */
+  public static function type($var): void
+  {
+    var_dump($var);
+    exit;
+  }
 }
