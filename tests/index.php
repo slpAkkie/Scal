514c6a2404c0d97ff8c71e334b4ecda67a45f28f
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-11-12 15:09:45 +0300

    Move Test class to submodule

--- tests/index.php
+++ tests/index.php
@@ -1,18 +1,11 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', true);
-ini_set('display_startup_errors', true);
-ini_set('html_errors', true);
-
-
-
 define('APP_ROOT_PATH', __DIR__);
 
-require_once(__DIR__ . '/Test.php');
-require_once(__DIR__ . '/../Initializer.php');
+require_once APP_ROOT_PATH . '/TestModule/options.php';
+require_once APP_ROOT_PATH . '/../Initializer.php';
 
-use Tests\Test;
+use TestModule\Test;
 
 
 
