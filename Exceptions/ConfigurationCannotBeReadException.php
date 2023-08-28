20601dbab525ae5cdf9a4bae6dae2c0be70fd848
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2023-08-28 09:18:51 +0300

    Update ConfigurationCannotBeReadException.php

--- Exceptions/ConfigurationCannotBeReadException.php
+++ Exceptions/ConfigurationCannotBeReadException.php
@@ -7,8 +7,8 @@ class ConfigurationCannotBeReadException extends \Exception
     protected $message = 'Configuration cannot be read';
     protected $code = E_USER_ERROR;
 
-    public function __construct(array|null $errorInfo, \Throwable $previous = null)
+    public function __construct(string $message, \Throwable $previous = null)
     {
-        parent::__construct($this->message . ' [' . $errorInfo['message'] . ']', $this->code, $previous);
+        parent::__construct($this->message . ' [' . $message . ']', $this->code, $previous);
     }
 }
